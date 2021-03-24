c666f7c676622851e60c3e69b221995ae314f2f9
Sebastian Bolanos <sebasheston@protonmail.com>
2021-03-24 21:23:37 +0000

    Clean up multiaddr

--- main.go
+++ main.go
@@ -13,11 +13,7 @@ import (
 
 func node(opts ...libp2p.Option) {
 	fmt.Printf("")
-	hostAddr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:60006")
-	if err != nil {
-		panic(err)
-	}
-	extAddr, err := net.ResolveTCPAddr("tcp", "0.0.0.0:60006")
+	hostAddr, err := net.ResolveTCPAddr("tcp", "localhost:60006")
 	if err != nil {
 		panic(err)
 	}
@@ -26,25 +22,17 @@ func node(opts ...libp2p.Option) {
 	if err != nil {
 		panic(err)
 	}
-	multiAddresses = append(multiAddresses, hostAddrMA)
-
-	if extAddr != nil {
-		extAddrMA, err := manet.FromNetAddr(extAddr)
-		if err != nil {
-			panic(err)
-		}
-		multiAddresses = append(multiAddresses, extAddrMA)
-	}
 
 	key := "bbf358ab08ab29d70b6b20845e4aa417124bb8051ecdbaf4f822bba18f28f7fb"
-	privKey, _ := ethNodeCrypto.HexToECDSA(key)
+	privKey, err := ethNodeCrypto.HexToECDSA(key)
+	if err != nil {
+		panic(err)
+	}
 	nodeKey := crypto.PrivKey((*crypto.Secp256k1PrivateKey)(privKey))
-	ctx := context.Background()
-
 	host, err := libp2p.New(
-		ctx,
+		context.Background(),
 		append(opts,
-			libp2p.ListenAddrs(multiAddresses...),
+			libp2p.ListenAddrs(append(multiAddresses, hostAddrMA)...),
 			libp2p.Identity(nodeKey),
 		)...,
 	)
